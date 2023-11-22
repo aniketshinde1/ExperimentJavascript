1a465d4bd423c2c8521502c000f723fc355b250d
aniket shinde <98145403+aniketshinde1@users.noreply.github.com>
2023-11-22 08:47:15 +0530

    Update practical4.js

--- practical4.js
+++ practical4.js
@@ -1,3 +1,4 @@
+Program 1: Using toUpperCase()
 // js program to perform string comparison
 const string1 = 'JavaScript Program';
 const string2 = 'javascript program';
@@ -8,3 +9,35 @@ if(result) {
 } else {
     console.log('The strings are not similar.');
 }
+
+Output
+The strings are similar.
+
+Program 2: JS String Comparison Using RegEx
+// program to perform string comparison
+const string1 = 'JavaScript Program';
+const string2 = 'javascript program';
+// create regex
+const pattern = new RegExp(string1, "gi");
+// compare the stings
+const result = pattern.test(string2)
+if(result) {
+    console.log('The strings are similar.');
+} else {
+    console.log('The strings are not similar.');
+}
+Output
+The strings are similar.
+
+Program 3: Using localeCompare()
+// program to perform case insensitive string comparison
+const string1 = 'JavaScript Program';
+const string2 = 'javascript program';
+const result = string1.localeCompare(string2, undefined, { sensitivity: 'base' });
+if(result == 0) {
+    console.log('The strings are similar.');
+} else {
+    console.log('The strings are not similar.');
+}
+Output
+The strings are similar
